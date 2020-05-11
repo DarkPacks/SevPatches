d04d93b628a7ee549ad304b66c300d423577a06b
Sam Kirby <sam.kirby94@hotmail.co.uk>
2020-05-11 12:26:43 +0100

    Add mixin library

--- src/main/java/tv/darkosto/sevpatches/core/SevPatchesLoadingPlugin.java
+++ src/main/java/tv/darkosto/sevpatches/core/SevPatchesLoadingPlugin.java
@@ -1,6 +1,10 @@
 package tv.darkosto.sevpatches.core;
 
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.Mixins;
 
 import javax.annotation.Nullable;
 import java.util.Map;
@@ -8,6 +12,14 @@ import java.util.Map;
 @IFMLLoadingPlugin.Name("SevPatches")
 @IFMLLoadingPlugin.MCVersion("1.12.2")
 public class SevPatchesLoadingPlugin implements IFMLLoadingPlugin {
+    public static Logger LOGGER = LogManager.getLogger("sevpatches_core");
+
+    public SevPatchesLoadingPlugin() {
+        LOGGER.info("setting up mixin environment");
+        MixinBootstrap.init();
+        Mixins.addConfiguration("mixins.sevpatches.json");
+    }
+
     @Override
     public String[] getASMTransformerClass() {
         return new String[]{"tv.darkosto.sevpatches.core.SevPatchesTransformer"};
